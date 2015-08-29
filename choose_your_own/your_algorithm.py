fcd57900040cfe3cbb73d4ba6ca18bca12c670c9
Dean Welch <deanwelch286@gmail.com>
2015-08-29 20:12:56 +0100

    completed up to lesson 4

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,7 +31,15 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import NearestCentroid
 
+clf = NearestCentroid()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+
+print accuracy_score(pred, labels_test)
 
 
 
